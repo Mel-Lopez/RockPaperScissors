f9bcad2a14a0da871cd96e7d11513cb7161a6b7f
Mel-Lopez <melvis.lopez228@gmail.com>
2023-05-31 18:54:42 -0500

    created getComputerFunction

--- rps.js
+++ rps.js
@@ -1,1 +1,16 @@
-console.log("Welcome to the Rock, Paper, Scissors Game!")
+console.log("Welcome to the Rock, Paper, Scissors Game!");
+
+function getComputerChoice () {
+    const choice = ["rock", "paper", "scissors"];
+    const randomChoice = Math.floor(Math.random() * choice.length) = 1;
+
+    if (randomChoice === 1) {
+        return "rock";
+    } else if (randomChoice === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+      }
+}
+
+console.log(getComputerChoice);
